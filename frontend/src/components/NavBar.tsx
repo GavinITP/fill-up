9e04b951af8913bd84f760059ea3cd54cec27ca0
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2024-10-12 18:52:12 +0700

    feat: add style to navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,9 @@
 "use client";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import Logo from "../../public/logo.svg";
+import Link from "next/link";
 
 export default function NavBar() {
   const pathName = usePathname();
@@ -13,8 +17,29 @@ export default function NavBar() {
   const isHidden = isUserLoginPage || isAdminLoginPage;
   return (
     !isHidden && (
-      <div className="sticky top-0 z-50 h-16 w-full bg-white shadow-md">
-        Nav Bar
+      <div className="sticky top-0 z-50 flex h-20 w-full flex-row items-center justify-between bg-white px-6 py-2 shadow-md">
+        <Link href={"/"}>
+          <Image
+            className="w-16"
+            width={Logo.width}
+            height={Logo.height}
+            src={Logo.src}
+            alt="Fill up"
+          />
+        </Link>
+        <div className="flex flex-row items-center gap-2">
+          <span className="text-5xl text-zinc-500">
+            <AccountCircleRoundedIcon fontSize="inherit" />
+          </span>
+          <div className="flex flex-col">
+            <span className="text-base font-normal text-black">
+              UserName Lastname
+            </span>
+            <span className="text-xs font-normal text-black">
+              ผู้ใช้/เจ้าของสถานีเติมน้ำ
+            </span>
+          </div>
+        </div>
       </div>
     )
   );
