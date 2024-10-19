70bcecc30e42bd750cb7c79985c1a17164f2a861
matikant <fe-2003@hotmail.com>
2024-10-19 10:18:23 +0700

    feat: frontend for water station management

--- frontend/src/components/Checkbox.tsx
+++ frontend/src/components/Checkbox.tsx
@@ -1,37 +1,37 @@
 import { ChangeEvent, Dispatch, SetStateAction } from "react";
 
-interface CheckboxProps {
-    label?: string;
-    value?: boolean;
-    setInputValue: Dispatch<SetStateAction<boolean>>;
+interface TextBoxProps {
+  title?: string;
+  setInputValue: Dispatch<SetStateAction<boolean>>;
+  disabled?: boolean;
+  subTitle?: string;
 }
 
-export default function Checkbox(
-    {
-        label,
-        value = false,
-        setInputValue,
-    }: CheckboxProps
-) {
-    return (
-        <div className="flex items-center justify-start gap-2">
-            <div className="inline-flex items-center">
-                <label className="flex items-center relative">
-                    <input
-                        type="checkbox"
-                        name={label}
-                        checked={value}
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => setInputValue(e.target.checked)}
-                        className="peer h-4 w-4 cursor-pointer transition-all appearance-none rounded hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800"
-                    />
-                    <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth="1">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                        </svg>
-                    </span>
-                </label>
-            </div>
-            {label && <h6 className="text-lg font-normal text-black">{label}</h6>}
-        </div>
-    )
+export default function CheckBox({
+  title,
+  setInputValue,
+  disabled = false,
+  subTitle,
+}: TextBoxProps) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.checked);
+  };
+  return (
+    <div className="flex w-full flex-row items-start gap-3">
+      <input
+        className="my-1.5 h-4 w-4 rounded-xl accent-black"
+        type="checkbox"
+        id="checkbox"
+        onChange={handleChange}
+        disabled={disabled}
+      ></input>
+
+      <div className="flex flex-col">
+        <label htmlFor="checkbox" className="text-lg text-black">
+          {title}
+        </label>
+        {subTitle && <div className="text-lg text-gray-400">{subTitle}</div>}
+      </div>
+    </div>
+  );
 }
