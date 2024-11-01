b1bfaf6b1beb22a0dc69c532973a3aa46c599e90
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-11-01 21:35:40 +0700

    feat: search

--- backend/services/water-station-service/src/routes/waterStation.routes.ts
+++ backend/services/water-station-service/src/routes/waterStation.routes.ts
@@ -3,16 +3,23 @@ import { waterStationController } from "../controllers/waterStation.controller";
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/")
-    .get(waterStationController.getWaterStations)
-    .post(waterStationController.createWaterStation);
+// Search water stations
+router.route("/search").get(waterStationController.searchWaterStations);
 
-router.route("/:id")
-    .get(waterStationController.getWaterStation)
-    .put(waterStationController.updateWaterStation)
-    .delete(waterStationController.deleteWaterStation);
+router
+  .route("/")
+  .get(waterStationController.getWaterStations)
+  .post(waterStationController.createWaterStation);
 
-router.route("/:id/update-approval-status").put(waterStationController.updateApprovalStatus)
-router.route("/:id/update-note").put(waterStationController.updateNote)
+router
+  .route("/:id")
+  .get(waterStationController.getWaterStation)
+  .put(waterStationController.updateWaterStation)
+  .delete(waterStationController.deleteWaterStation);
+
+router
+  .route("/:id/update-approval-status")
+  .put(waterStationController.updateApprovalStatus);
+router.route("/:id/update-note").put(waterStationController.updateNote);
 
 export default router;
