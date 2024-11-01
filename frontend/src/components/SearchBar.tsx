4ab2f73f7e1bf170d6fc9afb787fccccfb123c69
Ittipat Y <86824319+GavinITP@users.noreply.github.com>
2024-11-01 21:37:44 +0700

    feat: search (#6)

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,11 +1,28 @@
+"use client";
+
 import SearchIcon from "@mui/icons-material/Search";
 import FilterButton from "./FilterButton";
+import { useState } from "react";
 
-const SearchBar = () => {
+interface Props {
+  search: (query: unknown) => void;
+}
+
+const SearchBar = ({ search }: Props) => {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e: { target: { value: unknown } }) => {
+    const value = e.target.value;
+    setQuery(value);
+    search(value);
+  };
+
   return (
     <div className="relative mx-auto w-full">
       <input
         type="text"
+        value={query}
+        onChange={handleInputChange}
         className="w-full rounded-2xl border px-4 py-2 pr-20 shadow-sm focus:border-[#0288D1] focus:outline-none focus:ring-1 focus:ring-[#0288D1]"
         placeholder="ค้นหาสถานีเติมน้ำ"
       />
