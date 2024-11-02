d55d0ec9d094eac21f82b41fd0e7ca80ebf8e138
matikant <fe-2003@hotmail.com>
2024-11-02 10:14:46 +0700

    feat: link api for dashboard, edit each station, admin approve

--- backend/services/water-station-service/src/controllers/waterStation.controller.ts
+++ backend/services/water-station-service/src/controllers/waterStation.controller.ts
@@ -96,268 +96,330 @@ const getWaterStations = async (req: Request, res: Response) => {
 
     const waterStations = await query;
 
-    res.status(200).json({
-      success: true,
-      count: waterStations.length,
-      pagination,
-      data: waterStations,
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+    if (permission)
+      filter.permission = { $in: (permission as string).split(',') };
 
-const getWaterStation = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const waterStation = await WaterStation.findById(req.params.id);
-    if (!waterStation) return res.status(400).json({ success: false });
+    if (waterTemperature)
+      filter.waterTemperature = { $in: (waterTemperature as string).split(',') };
 
-    res.status(200).json({ success: true, data: waterStation });
-  } catch (err) {
-    res.status(404).json({ success: false });
-  }
-};
+    if (isFree)
+      filter.isFree = isFree === 'true';
 
-const createWaterStation = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    req.body.location = {
-      type: "Point",
-      coordinates: [req.body.longitude, req.body.latitude],
-    };
-    const createdWaterStation = await WaterStation.create(req.body);
-    res.status(201).json({ success: true, data: createdWaterStation });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+    if (approvalStatus)
+      filter.approvalStatus = approvalStatus;
 
-const updateWaterStation = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    req.body.updatedAt = new Date();
-    const waterStation = await WaterStation.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+    if (owner)
+      filter.owner = owner;
 
-    if (!waterStation) return res.status(400).json({ success: false });
+    query = WaterStation.find(filter)
 
-    res.status(200).json({ success: true, data: waterStation });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ success: false });
-  }
-};
-
-const updateApprovalStatus = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const filter = { _id: req.params.id };
-    const changedStatus = {
-      $set: { approvalStatus: req.body.isApproved ? "approved" : "rejected" },
-    };
-
-    req.body.updatedAt = new Date();
-    const result = await WaterStation.updateOne(filter, changedStatus);
-
-    if (result.matchedCount === 0) {
-      return res.status(400).json({ success: false });
+    if (req.query.select) {
+      const fields = (req.query.select as string).split(',').join(' ');
+      query = query.select(fields);
     }
 
-    // send email to the owner
-    const waterStation = await WaterStation.findById(req.params.id);
-    if (waterStation) {
-      const message = JSON.stringify({
-        waterStationName: waterStation.name,
-        waterStationStatus: req.body.isApproved ? "approved" : "rejected",
-        name: "Soft-Arch User",
-        email: "6432142321@student.chula.ac.th"
+    //Sort
+    if (req.query.sort) {
+      const sortBy = (req.query.sort as string).split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //Pagination
+    const page = parseInt(req.query.page as string, 10) || 1;
+    const limit = parseInt(req.query.limit as string, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    try {
+      const total = await WaterStation.countDocuments();
+
+      //Pagination result
+      const pagination = { next: {}, prev: {} };
+
+      if (limit !== -1) {
+        query = query.skip(startIndex).limit(limit);
+
+        if (endIndex < total) {
+          pagination.next = {
+            page: page + 1,
+            limit,
+          };
+        }
+
+        if (startIndex > 0) {
+          pagination.prev = {
+            page: page - 1,
+            limit,
+          };
+        }
+      }
+
+      const waterStations = await query;
+
+      res.status(200).json({
+        success: true,
+        count: waterStations.length,
+        pagination,
+        data: waterStations,
       });
-
-      sendMessageToQueue('water-station-approval', message);
+    } catch (err) {
+      res.status(400).json({ success: false });
     }
+  };
 
-    res.status(200).json({ success: true, data: result });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ success: false });
-  }
-};
+  const getWaterStation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const waterStation = await WaterStation.findById(req.params.id);
+      if (!waterStation) return res.status(400).json({ success: false });
 
-const updateNote = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const filter = { _id: req.params.id };
-    const note = {
-      $set: { note: req.body.note },
-    };
-
-    req.body.updatedAt = new Date();
-    const result = await WaterStation.updateOne(filter, note);
-
-    if (result.matchedCount === 0) {
-      return res.status(400).json({ success: false });
+      res.status(200).json({ success: true, data: waterStation });
+    } catch (err) {
+      res.status(404).json({ success: false });
     }
+  };
 
-    res.status(200).json({ success: true, data: result });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ success: false });
-  }
-};
+  const createWaterStation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      req.body.location = {
+        type: "Point",
+        coordinates: [req.body.longitude, req.body.latitude],
+      };
+      const createdWaterStation = await WaterStation.create(req.body);
+      res.status(201).json({ success: true, data: createdWaterStation });
+    } catch (err) {
+      res.status(400).json({ success: false });
+    }
+  };
 
-const deleteWaterStation = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const waterStation = await WaterStation.findById(req.params.id);
+  const updateWaterStation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      req.body.updatedAt = new Date();
+      const waterStation = await WaterStation.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
 
-    if (!waterStation) return res.status(404).json({ success: false });
+      if (!waterStation) return res.status(400).json({ success: false });
 
-    await waterStation.deleteOne();
+      res.status(200).json({ success: true, data: waterStation });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ success: false });
+    }
+  };
 
-    res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  const updateApprovalStatus = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const filter = { _id: req.params.id };
+      const changedStatus = {
+        $set: { approvalStatus: req.body.isApproved ? "approved" : "rejected" },
+      };
 
-const searchWaterStations = async (req: Request, res: Response) => {
-  let query;
+      req.body.updatedAt = new Date();
+      const result = await WaterStation.updateOne(filter, changedStatus);
 
-  const {
-    name,
-    latitude,
-    longitude,
-    address,
-    permission,
-    waterTemperature,
-    isFree,
-    approvalStatus,
-    minTemperature,
-    maxTemperature,
-  } = req.query;
-
-  const filter: any = {};
-
-  if (name && (name as string).length > 0) {
-    filter.name = { $regex: name, $options: "i" };
-  }
-
-  // Geo-location search (within a 2 km radius)
-  if (latitude && longitude) {
-    const latCenter = Number(latitude);
-    const longCenter = Number(longitude);
-    filter.location = {
-      $geoWithin: {
-        $centerSphere: [
-          [longCenter, latCenter], // [longitude, latitude]
-          2 / 6378.1, // radius in radians (2 km)
-        ],
-      },
-    };
-  }
-
-  // Address search (case-insensitive)
-  if (address) filter.address = { $regex: address, $options: "i" };
-
-  // Permission filtering
-  if (permission)
-    filter.permission = { $in: (permission as string).split(",") };
-
-  // Water temperature filtering (optional range filter)
-  if (waterTemperature) {
-    filter.waterTemperature = { $in: (waterTemperature as string).split(",") };
-  } else if (minTemperature || maxTemperature) {
-    filter.waterTemperature = {};
-    if (minTemperature) filter.waterTemperature.$gte = Number(minTemperature);
-    if (maxTemperature) filter.waterTemperature.$lte = Number(maxTemperature);
-  }
-
-  // Free station filtering
-  if (isFree) filter.isFree = isFree === "true";
-
-  // Approval status filtering
-  if (approvalStatus) filter.approvalStatus = approvalStatus;
-
-  query = WaterStation.find(filter);
-
-  // Field selection
-  if (req.query.select) {
-    const fields = (req.query.select as string).split(",").join(" ");
-    query = query.select(fields);
-  }
-
-  // Sort results
-  if (req.query.sort) {
-    const sortBy = (req.query.sort as string).split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-
-  // Pagination
-  const page = parseInt(req.query.page as string, 10) || 1;
-  const limit = parseInt(req.query.limit as string, 10) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  try {
-    const total = await WaterStation.countDocuments();
-
-    // Pagination result
-    const pagination: any = {};
-    if (limit !== -1) {
-      query = query.skip(startIndex).limit(limit);
-
-      if (endIndex < total) {
-        pagination.next = { page: page + 1, limit };
+      if (result.matchedCount === 0) {
+        return res.status(400).json({ success: false });
       }
 
-      if (startIndex > 0) {
-        pagination.prev = { page: page - 1, limit };
+      // send email to the owner
+      const waterStation = await WaterStation.findById(req.params.id);
+      if (waterStation) {
+        const message = JSON.stringify({
+          waterStationName: waterStation.name,
+          waterStationStatus: req.body.isApproved ? "approved" : "rejected",
+          name: "Soft-Arch User",
+          email: "6432142321@student.chula.ac.th"
+        });
+
+        sendMessageToQueue('water-station-approval', message);
       }
+
+      res.status(200).json({ success: true, data: result });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ success: false });
+    }
+  };
+
+  const updateNote = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const filter = { _id: req.params.id };
+      const note = {
+        $set: { note: req.body.note },
+      };
+
+      req.body.updatedAt = new Date();
+      const result = await WaterStation.updateOne(filter, note);
+
+      if (result.matchedCount === 0) {
+        return res.status(400).json({ success: false });
+      }
+
+      res.status(200).json({ success: true, data: result });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ success: false });
+    }
+  };
+
+  const deleteWaterStation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const waterStation = await WaterStation.findById(req.params.id);
+
+      if (!waterStation) return res.status(404).json({ success: false });
+
+      await waterStation.deleteOne();
+
+      res.status(200).json({ success: true, data: {} });
+    } catch (err) {
+      res.status(400).json({ success: false });
+    }
+  };
+
+  const searchWaterStations = async (req: Request, res: Response) => {
+    let query;
+
+    const {
+      name,
+      latitude,
+      longitude,
+      address,
+      permission,
+      waterTemperature,
+      isFree,
+      approvalStatus,
+      minTemperature,
+      maxTemperature,
+    } = req.query;
+
+    const filter: any = {};
+
+    if (name && (name as string).length > 0) {
+      filter.name = { $regex: name, $options: "i" };
     }
 
-    const waterStations = await query;
+    // Geo-location search (within a 2 km radius)
+    if (latitude && longitude) {
+      const latCenter = Number(latitude);
+      const longCenter = Number(longitude);
+      filter.location = {
+        $geoWithin: {
+          $centerSphere: [
+            [longCenter, latCenter], // [longitude, latitude]
+            2 / 6378.1, // radius in radians (2 km)
+          ],
+        },
+      };
+    }
 
-    res.status(200).json({
-      success: true,
-      count: waterStations.length,
-      pagination,
-      data: waterStations,
-    });
-  } catch (err) {
-    res.status(400).json({ success: false, error: (err as Error).message });
-  }
-};
+    // Address search (case-insensitive)
+    if (address) filter.address = { $regex: address, $options: "i" };
 
-export const waterStationController = {
-  getWaterStations,
-  getWaterStation,
-  createWaterStation,
-  updateWaterStation,
-  updateApprovalStatus,
-  updateNote,
-  deleteWaterStation,
-  searchWaterStations,
-};
+    // Permission filtering
+    if (permission)
+      filter.permission = { $in: (permission as string).split(",") };
+
+    // Water temperature filtering (optional range filter)
+    if (waterTemperature) {
+      filter.waterTemperature = { $in: (waterTemperature as string).split(",") };
+    } else if (minTemperature || maxTemperature) {
+      filter.waterTemperature = {};
+      if (minTemperature) filter.waterTemperature.$gte = Number(minTemperature);
+      if (maxTemperature) filter.waterTemperature.$lte = Number(maxTemperature);
+    }
+
+    // Free station filtering
+    if (isFree) filter.isFree = isFree === "true";
+
+    // Approval status filtering
+    if (approvalStatus) filter.approvalStatus = approvalStatus;
+
+    query = WaterStation.find(filter);
+
+    // Field selection
+    if (req.query.select) {
+      const fields = (req.query.select as string).split(",").join(" ");
+      query = query.select(fields);
+    }
+
+    // Sort results
+    if (req.query.sort) {
+      const sortBy = (req.query.sort as string).split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // Pagination
+    const page = parseInt(req.query.page as string, 10) || 1;
+    const limit = parseInt(req.query.limit as string, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    try {
+      const total = await WaterStation.countDocuments();
+
+      // Pagination result
+      const pagination: any = {};
+      if (limit !== -1) {
+        query = query.skip(startIndex).limit(limit);
+
+        if (endIndex < total) {
+          pagination.next = { page: page + 1, limit };
+        }
+
+        if (startIndex > 0) {
+          pagination.prev = { page: page - 1, limit };
+        }
+      }
+
+      const waterStations = await query;
+
+      res.status(200).json({
+        success: true,
+        count: waterStations.length,
+        pagination,
+        data: waterStations,
+      });
+    } catch (err) {
+      res.status(400).json({ success: false, error: (err as Error).message });
+    }
+  };
+
+  export const waterStationController = {
+    getWaterStations,
+    getWaterStation,
+    createWaterStation,
+    updateWaterStation,
+    updateApprovalStatus,
+    updateNote,
+    deleteWaterStation,
+    searchWaterStations,
+  };
