661742cfe92921e2b1bf8d95f0f51a99173001cf
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-11-04 16:51:17 +0700

    feat: improve gateway security and fix responsive design

--- backend/gateway/src/index.ts
+++ backend/gateway/src/index.ts
@@ -1,25 +1,34 @@
-import express from 'express';
-import proxy from 'express-http-proxy';
-import dotenv from 'dotenv';
+import express from "express";
+import proxy from "express-http-proxy";
+import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+import helmet from "helmet";
+
+const reports = require("./routes/reports");
 
 dotenv.config();
 
-const reports = require('./routes/reports');
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, please try again later.",
+});
 
 const app = express();
 app.use(express.json());
+app.use(limiter);
+app.use(cors());
+app.use(helmet());
+
+const USER_SERVICE_URL = process.env.USER_SERVICE_URL || "";
+const WATER_STATION_SERVICE_URL = process.env.WATER_STATION_SERVICE_URL || "";
+
+app.use("/report-service", reports);
+app.use("/user", proxy(USER_SERVICE_URL));
+app.use("/water-station", proxy(WATER_STATION_SERVICE_URL));
 
 const PORT = process.env.PORT || 8080;
-
-const MAIL_SERVICE_URL = process.env.MAIL_SERVICE_URL || '';
-const USER_SERVICE_URL = process.env.USER_SERVICE_URL || '';
-const WATER_STATION_SERVICE_URL = process.env.WATER_STATION_SERVICE_URL || '';
-
-// app.use("/mail", proxy(MAIL_SERVICE_URL));
-app.use('/reportservice', reports);
-app.use('/user', proxy(USER_SERVICE_URL));
-app.use('/water-station', proxy(WATER_STATION_SERVICE_URL));
-
 app.listen(PORT, () => {
-  console.log(`API Gateway running on port ${PORT}`);
+  console.log(`Gateway is running on port ${PORT}`);
 });
