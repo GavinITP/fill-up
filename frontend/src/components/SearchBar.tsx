5280b4f510748812c1c40c530eb8d3176c88f1a6
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-11-12 00:02:26 +0700

    fix: debouncing search

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -4,9 +4,11 @@ import SearchIcon from "@mui/icons-material/Search";
 import FilterButton from "./FilterButton";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useDebounce } from "use-debounce";
 
 const SearchBar = () => {
   const [query, setQuery] = useState("");
+  const [debouncedQuery] = useDebounce(query, 200);
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -15,25 +17,25 @@ const SearchBar = () => {
     setQuery(initialQuery);
   }, [searchParams]);
 
-  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const value = e.target.value;
-    setQuery(value);
-
+  useEffect(() => {
     const params = new URLSearchParams(searchParams.toString());
-    if (value) {
-      params.set("search", value);
+    if (debouncedQuery) {
+      params.set("search", debouncedQuery);
     } else {
       params.delete("search");
     }
-
     router.replace(`?${params.toString()}`);
+  }, [debouncedQuery, router, searchParams]);
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(e.target.value);
   }
 
   return (
     <div className="relative mx-auto w-full">
       <input
         type="text"
-        value={query}
+        defaultValue={query}
         onChange={handleInputChange}
         className="w-full rounded-2xl border px-4 py-2 pr-20 shadow-sm focus:border-[#0288D1] focus:outline-none focus:ring-1 focus:ring-[#0288D1]"
         placeholder="ค้นหาสถานีเติมน้ำ"
