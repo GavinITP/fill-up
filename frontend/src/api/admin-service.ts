6bf9a17c237b58bce88a46f4349f4f4eb8e492cf
matikant <fe-2003@hotmail.com>
2024-11-12 16:27:54 +0700

    fix: admin see report

--- frontend/src/api/admin-service.ts
+++ frontend/src/api/admin-service.ts
@@ -52,5 +52,60 @@ export const adminService = {
             throw new Error("Failed to get owner email and name");
         }
         return await response.json();
+    },
+
+    getReports: async (token: string) => {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}/report-service/reports`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            },
+        );
+        if (!response.ok) {
+            throw new Error("Failed to get reports");
+        }
+        return await response.json();
+    },
+
+    markReport: async (token: string, reportId: string, completed: boolean) => {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}/report-service/report/${reportId}/complete`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    completed: completed
+                }),
+            },
+        );
+        if (!response.ok) {
+            throw new Error("Failed to mark report");
+        }
+        return await response.json();
+    },
+
+    sendAdminNote: async (token: string, stationId: string, note: string) => {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}/water-station/${stationId}/update-note`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    note: note
+                }),
+            },
+        );
+        if (!response.ok) {
+            throw new Error("Failed to send comment");
+        }
+        return await response.json();
     }
 };
